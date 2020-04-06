805aa85b629d7ca1b472333c769271d49118d8b9
Justin Miller <indymiller@gmail.com>
2020-04-06 16:46:04 -0400

    added parser and debugger testing

--- dirwatcher.py
+++ dirwatcher.py
@@ -5,12 +5,34 @@ __author__ = "Justin Miller"
 import sys
 import logging
 import datetime
-
+import time
+import argparse
 
 if(sys.version_info[0] < 3):
     raise RuntimeError('This program requires Python 3+')
 
 logger = logging.getLogger(__file__)
+
+def watch_directory(args):
+    logger.info('Watching directory: {}, File Extention: {}, Polling Interval: {}, Magic Text: {}'.format(
+        args.path, args.ext, args.interval, args.magic
+    ))
+    while True:
+        try:
+            logger.info('Inside Watch Loop')
+            time.sleep(args.interval)
+        except KeyboardInterrupt:
+            break
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--ext', type=str, default='.txt',
+                        help='Text file extension to watch')
+    parser.add_argument('-i', '--interval', type=float,
+                        default=1.0, help='Number of seconda between polling')
+    parser.add_argument('path', help='Directory path to watch')
+    parser.add_argument('magic', help='String to watch for')
+    return parser
 
 def main():
     logging.basicConfig(
@@ -27,6 +49,9 @@ def main():
         '------------------------------------------------------\n'
         .format(__file__, app_start_time.isoformat())
     )
+    parser = create_parser()
+    args = parser.parse_args()
+    watch_directory(args) 
     uptime = datetime.datetime.now()-app_start_time
     logger.info(
         '\n'
