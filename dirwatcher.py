1c42c28fc1b56df03e39e85709f4f710969b3640
Justin Miller <indymiller@gmail.com>
2020-04-06 17:02:55 -0400

    added gitignore

--- dirwatcher.py
+++ dirwatcher.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 
-__author__ = "Justin Miller"
+__author__ = "Justin Miller and demo from 3/25"
 
 import sys
 import logging
 import datetime
 import time
 import argparse
+import os
+
 
 if(sys.version_info[0] < 3):
     raise RuntimeError('This program requires Python 3+')
 
 logger = logging.getLogger(__file__)
 
+def find_magic(filename, starting_line, magic_word):
+    pass
+
 def watch_directory(args):
+    watching_files: {}
     logger.info('Watching directory: {}, File Extention: {}, Polling Interval: {}, Magic Text: {}'.format(
         args.path, args.ext, args.interval, args.magic
     ))
+    #Keys are actual filenames and values are last line in file read
+ 
+    #Look at directory and get a list of files from it
+    #Put files into watching_files dictionary if not already in
+    #Log new files that are added to dictionary
+    #Loop thru dictionary and compare to list of files in directory
+    #If file is misssing from directory, remove from dictionary
+    #For all remaining files in dictionary, open each file at last line that was read from and look for "magic" text
+    #Log any new magic words located
+    #Update last postition of where you were in files
+
     while True:
         try:
             logger.info('Inside Watch Loop')
